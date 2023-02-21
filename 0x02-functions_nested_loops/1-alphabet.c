140bf6661a87650023298e7ceb26f3a6ed835598
Codenerd1 <muhiageorge98@gmail.com>
2023-02-21 13:49:22 -0800

    Task 1 Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Function to make the alphabet
+ * 
+ * Return: void (sucess)
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for(c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
