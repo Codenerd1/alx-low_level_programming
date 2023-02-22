04f6d5fa66ff9cd64bd393cf624e6bd2d6f8ae23
Codenerd1 <muhiageorge98@gmail.com>
2023-02-22 13:20:05 -0800

    Task 14 Revised

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,14 +2,13 @@
 
 /**
  * main - Prints the first 52 fibonacci numbers
- *
- * Return: Nothing!
+ * Return: Nothing
  */
 
 int main(void)
 {
 	int i = 0;
-	long j = 1, k =2;
+	long j = 1, k = 2;
 
 	while (i < 50)
 	{
