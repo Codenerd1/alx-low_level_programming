f5ad61049aaeae477f98cf1b3c12bf0b5ebb4db4
Codenerd1 <muhiageorge98@gmail.com>
2023-03-09 11:03:30 -0800

    Task6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_chessboard - Entry point
@@ -12,7 +13,7 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (n = 0; n < 8; n++)
-			_putchar(a[i][n]);
-		_putchar('\n');
+			putchar(a[i][n]);
+		putchar('\n');
 	}
 }
